0c650d03b47b68257d65d20864bad0883b916309
Monarene <mekuleyimichael@gmail.com>
2021-02-02 23:29:30 +0100

    bar chart shows 10

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_budget_ui/data/data.dart';
+import 'package:flutter_budget_ui/models/category_model.dart';
+import 'package:flutter_budget_ui/models/expense_model.dart';
+import 'package:flutter_budget_ui/widgets/BarChart.dart';
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -31,29 +35,38 @@ class _HomeScreenState extends State<HomeScreen> {
               )
             ],
           ),
-          // SliverList(
-          //   delegate:
-          //       SliverChildBuilderDelegate((BuildContext context, int index) {
-          //     if (index == 0) {
-          //       return Container(
-          //         margin: EdgeInsets.fromLTRB(20, 20, 20, 10),
-          //         decoration: BoxDecoration(
-          //             color: Colors.white,
-          //             boxShadow: [
-          //               BoxShadow(
-          //                   color: Colors.black12,
-          //                   offset: Offset(0, 2),
-          //                   blurRadius: 6.0)
-          //             ],
-          //             borderRadius: BorderRadius.circular(10.0)),
-          //       );
-          //     } else {
-          //       return SizedBox(
-          //         height: 0,
-          //       );
-          //     }
-          //   }),
-          // )
+          SliverList(
+            delegate:
+                SliverChildBuilderDelegate((BuildContext context, int index) {
+              if (index == 0) {
+                return Container(
+                  margin: EdgeInsets.fromLTRB(20, 20, 20, 10),
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      boxShadow: [
+                        BoxShadow(
+                            color: Colors.black12,
+                            offset: Offset(0, 2),
+                            blurRadius: 6.0)
+                      ],
+                      borderRadius: BorderRadius.circular(10.0)),
+                  child: BarChart(
+                    expenses: weeklySpending,
+                  ),
+                );
+              } else {
+                final Category category = categories[index - 1];
+                double totalAmountSpent = 0;
+                category.expenses.forEach((Expense element) {
+                  totalAmountSpent += element.cost;
+                });
+
+                return SizedBox(
+                  height: 0,
+                );
+              }
+            }, childCount: 1 + categories.length),
+          )
         ],
       ),
     );
