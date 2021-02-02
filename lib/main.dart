e0e236b5b7e6770e0a9627dafedb628489888b52
Monarene <mekuleyimichael@gmail.com>
2021-02-02 01:35:20 +0100

    setup the solver app bar 5

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Budget UI',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.green,
         visualDensity: VisualDensity.adaptivePlatformDensity,
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -9,8 +9,52 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text("I am here"),
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            forceElevated: true,
+            floating: true,
+            expandedHeight: 100.0,
+            leading: IconButton(
+              icon: Icon(Icons.settings),
+              iconSize: 30.0,
+              onPressed: () {},
+            ),
+            flexibleSpace: FlexibleSpaceBar(
+              title: Text("Simple Budget"),
+            ),
+            actions: [
+              IconButton(
+                icon: Icon(Icons.add),
+                iconSize: 30.0,
+                onPressed: () {},
+              )
+            ],
+          ),
+          SliverList(
+            delegate:
+                SliverChildBuilderDelegate((BuildContext context, int index) {
+              if (index == 0) {
+                return Container(
+                  margin: EdgeInsets.fromLTRB(20, 20, 20, 10),
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      boxShadow: [
+                        BoxShadow(
+                            color: Colors.black12,
+                            offset: Offset(0, 2),
+                            blurRadius: 6.0)
+                      ],
+                      borderRadius: BorderRadius.circular(10.0)),
+                );
+              } else {
+                return SizedBox(
+                  height: 0,
+                );
+              }
+            }),
+          )
+        ],
       ),
     );
   }
