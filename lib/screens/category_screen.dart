baed9f2eb8f0d63175ce0160682fd9b511cc9fc3
Monarene <mekuleyimichael@gmail.com>
2021-02-03 14:49:00 +0100

    added the build method  3

---
+++ lib/screens/category_screen.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_budget_ui/models/category_model.dart';
+import 'package:flutter_budget_ui/models/expense_model.dart';
+
+class CatergoryScreen extends StatefulWidget {
+  final Category catergory;
+  CatergoryScreen({this.catergory});
+
+  @override
+  _CatergoryScreenState createState() => _CatergoryScreenState();
+}
+
+class _CatergoryScreenState extends State<CatergoryScreen> {
+  _buildExpenses() {
+    List<Widget> expenseList = [];
+    widget.catergory.expenses.forEach((Expense expense) {
+      expenseList.add(Container(
+        alignment: Alignment.center,
+        margin: EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
+        height: 80.0,
+        width: double.infinity,
+        decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(10),
+            boxShadow: [
+              BoxShadow(
+                  offset: Offset(0, 2), color: Colors.black12, blurRadius: 6.0)
+            ]),
+        child: Padding(
+          padding: EdgeInsets.all(30),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                expense.name,
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text(
+                '-\$${expense.cost.toStringAsFixed(2)}',
+                style: TextStyle(
+                    color: Colors.red,
+                    fontSize: 20,
+                    fontWeight: FontWeight.w600),
+              )
+            ],
+          ),
+        ),
+      ));
+    });
+
+    return Column(
+      children: expenseList,
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
